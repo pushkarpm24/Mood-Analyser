a9305c28d095c5a2e1ff0f09f8eb64258cb03b73
pushkarpm24 <pushkarmorey555@gmail.com>
2020-06-30 07:22:58 +0530

    ADDED: test to check the sad mood

---
+++ MoodAnalyser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalysis
+
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public string AnalyseMood(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "SAD";               
+            }
+
+            else
+            {
+                return "Happy";
+            }
+        }
+
+    }
+}
